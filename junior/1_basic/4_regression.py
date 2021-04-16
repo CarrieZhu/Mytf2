e58e2cf5d7d2765fe9590552fb6509a517f3afa4
Kaiyu KY2 Zhu <zhuky2@Lenovo.com>
2021-04-16 18:44:26 +0800

    4

--- junior/1_basic/4_regression.py
+++ junior/1_basic/4_regression.py
@@ -12,6 +12,17 @@ from tensorflow.keras import layers
 
 print(tf.__version__)
 
+# =============================================================================
+# 本笔记本 (notebook) 介绍了一些处理回归问题的技术。
+# 
+# 均方误差（MSE）是用于回归问题的常见损失函数（分类问题中使用不同的损失函数）。
+# 类似的，用于回归的评估指标与分类不同。 常见的回归指标是平均绝对误差（MAE）。
+# 当数字输入数据特征的值存在不同范围时，每个特征应独立缩放到相同范围。
+# 如果训练数据不多，一种方法是选择隐藏层较少的小网络，以避免过度拟合。
+# 早期停止是一种防止过度拟合的有效技术。
+# =============================================================================
+
+
 #%%
 # 获取数据
 dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
@@ -39,18 +50,135 @@ print(dataset.tail())
 
 # %%
 # 拆分训练数据集和测试数据
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+#%%
+# 数据检查
+sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
+#%%
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+print(train_stats)
 
 
+#%%
+# 从标签中分离特征
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+
+#%%
+# 数据规范化: x = (x - mean) / std
+# 警告: 用于归一化输入的数据统计（均值和标准差）需要反馈给模型从而应用于任何其他数据，以及我们之前所获得独热码。
+# 这些数据包含测试数据集以及生产环境中所使用的实时数据。
+def norm(x):
+    return (x - train_stats['mean']) / train_stats['std']
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+#%%
+# 构建模型
+
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1)
+        ])
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae', 'mse'])
+    return model
+model = build_model()
+model.summary()
+#%%
+example_batch = normed_train_data[:10]
+example_result = model.predict(example_batch)
+print(example_result)
+
+#%%
+# 训练模型
+# 通过为每个完成的时期打印一个点来显示训练进度
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
+
+EPOCHS = 1000
+
+history = model.fit(
+    normed_train_data, train_labels,
+    epochs=EPOCHS, validation_split=0.2, verbose=0,
+    callbacks=[PrintDot()])
+
+#%%
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+print(hist.tail())
 
 
+def plot_history(history):
+  hist = pd.DataFrame(history.history)
+  hist['epoch'] = history.epoch
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Abs Error [MPG]')
+  plt.plot(hist['epoch'], hist['mae'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mae'],
+           label = 'Val Error')
+  plt.ylim([0,5])
+  plt.legend()
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Square Error [$MPG^2$]')
+  plt.plot(hist['epoch'], hist['mse'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mse'],
+           label = 'Val Error')
+  plt.ylim([0,20])
+  plt.legend()
+  plt.show()
 
 
+plot_history(history)
 
 
+#%%
+# EarlyStopping callback 
 
+model = build_model()
 
+# patience 值用来检查改进 epochs 的数量
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
+history = model.fit(normed_train_data, train_labels, epochs=EPOCHS,
+                    validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
 
+plot_history(history)
 
+#%%
+# 测试集
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
 
+print("Testing set Mean Abs Error: {:5.2f} MPG".format(mae))
 
+#%%
+test_predictions = model.predict(normed_test_data).flatten()
+
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+plt.axis('equal')
+plt.axis('square')
+plt.xlim([0,plt.xlim()[1]])
+plt.ylim([0,plt.ylim()[1]])
+_ = plt.plot([-100, 100], [-100, 100])
+#%%
+error = test_predictions - test_labels
+plt.hist(error, bins = 25)
+plt.xlabel("Prediction Error [MPG]")
+_ = plt.ylabel("Count")
